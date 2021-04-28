eaede8264ed949e8e5a81fba84f9d30fb1b33da3
Danny Tram <tramdanny@hotmail.com>
2021-04-28 09:56:09 -0700

    fixed font src bug, added js scripts for main and shows

---
+++ sprint-two/scripts/shows.js
@@ -0,0 +1,32 @@
+let showsArray = [
+    {
+        dates = "Mon Sept 06 2021",
+        venue = "Ronald Lane",
+        location = "San Francisco, CA",
+    },
+    {
+        dates = "Tue Sept 21 2021",
+        venue = "Pier 3 East",
+        location = "San Francisco, CA",
+    },
+    {
+        dates = "Fri Oct 15 2021",
+        venue = "View Lounge",
+        location = "San Francisco, CA",
+    },
+    {
+        dates = "Sat Nov 06 2021",
+        venue = "Hyatt Agency",
+        location = "San Francisco, CA",
+    },
+    {
+        dates = "Fri Nov 26 2021",
+        venue = "Moscow Center",
+        location = "San Francisco, CA",
+    },
+    {
+        dates = "Wed Dec 15 2021",
+        venue = "Press Club",
+        location = "San Francisco, CA",
+    },
+]
